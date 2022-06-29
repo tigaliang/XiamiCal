0e3f07b15925844419abb075550abac369af990b
Tiga Liang <liangkangabc@gmail.com>
2022-06-29 13:17:20 +0800

    Initial Commit

---
+++ XiamiCal/ContentView.swift
@@ -0,0 +1,17 @@
+// Created by Tiga Liang on 2022/6/29.
+// Copyright © 2022 Airbnb Inc. All rights reserved.
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ XiamiCal/XiamiCalApp.swift
@@ -0,0 +1,13 @@
+// Created by Tiga Liang on 2022/6/29.
+// Copyright © 2022 Airbnb Inc. All rights reserved.
+
+import SwiftUI
+
+@main
+struct XiamiCalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
